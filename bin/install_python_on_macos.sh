cf8a519a6e9fa2bba342abaa1a2e78b6f198bd46
Zach Moshe <zachmoshe@gmail.com>
2021-09-18 17:25:05 +0300

    Add an installation script for python and bump to 3.9.7

---
+++ bin/install_python_on_macos.sh
@@ -0,0 +1,18 @@
+# First make sure you have these in .bashrc
+# export PATH="/usr/local/opt/tcl-tk/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/tcl-tk/lib"
+# export CPPFLAGS="-I/usr/local/opt/tcl-tk/include"
+# export PKG_CONFIG_PATH="/usr/local/opt/tcl-tk/lib/pkgconfig"
+
+brew update
+brew install tcl-tk zlib readline xz bzip2
+
+python_version='3.9.7'
+env \
+  PATH="$(brew --prefix tcl-tk)/bin:$PATH" \
+  CFLAGS="-I$(brew --prefix tcl-tk)/include -I$(brew --prefix zlib)/include -I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include -I$(brew --prefix readline)/include -I$(xcrun --show-sdk-path)/usr/include" \
+  LDFLAGS="-L$(brew --prefix tcl-tk)/lib -L$(brew --prefix bzip2)/lib -L$(brew --prefix zlib) -L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib" \
+  CPPFLAGS="-I$(brew --prefix tcl-tk)/include -I$(brew --prefix bzip2)/include -I$(brew --prefix zlib)/include" \
+  PKG_CONFIG_PATH="$(brew --prefix tcl-tk)/lib/pkgconfig" \
+  PYTHON_CONFIGURE_OPTS="--with-tcltk-includes='-I$(brew --prefix tcl-tk)/include' --with-tcltk-libs='-L$(brew --prefix tcl-tk)/lib -ltcl8.6 -ltk8.6'" \
+  pyenv install $python_version
