037551bb38c1b033d98f1da1801a7a66b18e8ac9
Zach Moshe <zachmoshe@gmail.com>
2021-10-10 20:22:47 +0300

    changing plot_utils polygons to UTM. Adding ADM1 polygons.

--- il_elections/utils/plot_utils.py
+++ il_elections/utils/plot_utils.py
@@ -1,64 +1,89 @@
 """Utilities for plotting, mostly Colab and visualization stuff."""
-import dataclasses
 import enum
-from typing import Sequence
+import pathlib
 
 import branca
 import folium
 import geopandas as gpd
+import pandas as pd
+import pyproj
 import shapely
 
-from il_elections.utils import data_utils
+
+PROJ_UTM = 'EPSG:32636'  # UTM zone 36 (matches Israel)
+PROJ_LNGLAT = 'EPSG:4326'
+
+utm_to_lnglat = pyproj.Transformer.from_proj(
+    pyproj.Proj(PROJ_UTM), pyproj.Proj(PROJ_LNGLAT), always_xy=True)
+lnglat_to_utm = pyproj.Transformer.from_proj(
+    pyproj.Proj(PROJ_LNGLAT), pyproj.Proj(PROJ_UTM), always_xy=True)
 
 
-@dataclasses.dataclass
-class PointWithRadiusMapBounds:
-    """Represents map bounds from a center point and a radius."""
-    center: Sequence[float]  # (lng, lat)
-    radius_meters: float
-
-    @property
-    def bounds(self):
-        """Returns the bounds (min_lng, min_lat, max_lng, max_lat) of the map."""
-        bounds = (gpd.GeoSeries(shapely.geometry.Point(*self.center),
-                                crs=data_utils.DEGREE_PROJECTION)
-                  .to_crs(data_utils.EQUAL_AREA_METRIC_PROJECTION)
-                  .buffer(self.radius_meters)
-                  .to_crs(data_utils.DEGREE_PROJECTION)
-                  ).total_bounds
-        return tuple(bounds)
+_ISR_ADM0_GEOJSON_PATH = pathlib.Path('data/gis/boundaries/geoBoundaries-ISR-ADM0-all.zip')
+_ISR_ADM1_GEOJSON_PATH = pathlib.Path('data/gis/boundaries/geoBoundaries-ISR-ADM1-all.zip')
+_PSE_ADM0_GEOJSON_PATH = pathlib.Path('data/gis/boundaries/geoBoundaries-PSE-ADM0-all.zip')
+_PSE_ADM1_GEOJSON_PATH = pathlib.Path('data/gis/boundaries/geoBoundaries-PSE-ADM1-all.zip')
+_ISR_ADM1 = (
+    gpd.read_file(_ISR_ADM1_GEOJSON_PATH)
+    .set_index('shapeName')
+    .to_crs(PROJ_UTM))
 
 
-@dataclasses.dataclass
-class RectangleMapBounds:
-    """Represents arbitrary map bounds."""
-    bounds: Sequence[float]  # (min_lng, min_lat, max_lng, max_lat)
+def generate_circle_utm(center, radius_meters):
+    return shapely.geometry.Point(center).buffer(radius_meters)
 
-    @property
-    def center(self):
-        return tuple(self.bounds.reshape((2, 2)).mean(axis=0))
+
+def generate_rectangle_utm(min_x, min_y, max_x, max_y):
+    return shapely.geometry.Polygon(
+        [(min_x, min_y), (min_x, max_y), (max_x, max_y), (max_x, min_y)])
 
 
 class Maps(enum.Enum):
     """Holds known maps for easier reference."""
-    # Cities
-    TLV = PointWithRadiusMapBounds(center=[32.07319, 34.79328], radius_meters=2500)
-    HAIFA = PointWithRadiusMapBounds(center=[32.805037, 35.025720], radius_meters=5000)
-    JERUSALEM = PointWithRadiusMapBounds(center=[31.770260, 35.208957], radius_meters=5000)
-    BEERSHEVA = PointWithRadiusMapBounds(center=[31.252142, 34.792088], radius_meters=3500)
-
     # Areas
-    NORTH = RectangleMapBounds(bounds=(34.8101, 32.4171, 35.9143, 33.303))
-    CENTER = RectangleMapBounds(bounds=(34.552, 31.9102, 35.3045, 32.2685))
-    SOUTH = RectangleMapBounds(bounds=(34.2, 29.5, 35.6, 31.35))
-    ISRAEL = RectangleMapBounds(bounds=(34.2674, 29.4906, 35.8950, 33.3356))
+    ISRAEL = _ISR_ADM1.dissolve().iloc[0].geometry
+    NORTH = _ISR_ADM1.loc['North District'].geometry
+    CENTER = _ISR_ADM1.loc[['Tel Aviv District', 'Center District']].dissolve().iloc[0].geometry
+    SOUTH = _ISR_ADM1.loc['South District'].geometry
+
+    # Cities
+    BEERSHEVA = generate_circle_utm(center=(671041.49, 3458353.12), radius_meters=5000)
+    HAIFA = generate_circle_utm(center=(687075.41, 3631881.30), radius_meters=5000)
+    JERUSALEM = generate_circle_utm(center=(709616.69, 3517636.21), radius_meters=5000)
+    TLV = generate_circle_utm(center=(668017.45, 3550814.14), radius_meters=2500)
+
+    @property
+    def center(self):
+        return self.value.centroid
+
+    @property
+    def bounds(self):
+        return self.value.bounds
 
 
 def map(map_def, width='100%', height=400):  # pylint: disable=redefined-builtin
     """Generates a map object that plays nicely with Colab."""
-    map_def = map_def.value
     fig = branca.element.Figure(width=width, height=height)
     m = folium.Map(tiles='OpenStreetMap')
-    m.fit_bounds((map_def.bounds[:2][::-1], map_def.bounds[2:][::-1]))
+
+    lnglat_polygon = shapely.ops.transform(utm_to_lnglat.transform, map_def.value)
+    minx, miny, maxx, maxy = lnglat_polygon.bounds
+    # fit_bounds() requires (lat, lng) of southwest, northeast.
+    m.fit_bounds(((miny, minx), (maxy, maxx)))
+
     fig.add_child(m)
     return m
+
+
+def load_israel_polygon():
+    """Returns a Shapely polygon (UTM) for the state of Israel (including the west bank)."""
+    # Take all Israel and the West Bank only from PSE.
+    israel = pd.concat([
+        gpd.read_file(_ISR_ADM1_GEOJSON_PATH),
+        gpd.read_file(_PSE_ADM1_GEOJSON_PATH),
+    ]).query('shapeGroup=="ISR" or shapeISO=="PS-WBK"').unary_union
+
+    # Eliminate all tiny holes due to imperfect alignment between ISR and PSE files.
+    israel = shapely.geometry.Polygon(israel.exterior)
+    israel_utm = shapely.ops.transform(lnglat_to_utm.transform, israel)
+    return israel_utm
